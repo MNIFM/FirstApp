ff9aeb8907e77fcb95659fcd6a6b195348f91ae5
https://github.com/MNIFM/BlogApp <mahdi.mnif1@gmail.com>
2018-10-10 16:52:28 +0200

    First commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,55 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { FirstComponent } from './first/first.component';
+import { AppareilComponent } from './appareil/appareil.component';
+import { FormsModule } from '@angular/forms';
+import { AppareilService } from './services/appareil.service';
+import { AuthComponent } from './auth/auth.component';
+import { AppareilViewComponent } from './appareil-view/appareil-view.component';
+import { AuthService } from './services/auth.service';
+import { SingleAppareilComponent } from './single-appareil/single-appareil.component';
+import { FourOhFourComponent } from './four-oh-four/four-oh-four.component';
+import { AuthGuard } from './services/auth-guard.service';
+import { EditAppareilComponent } from './edit-appareil/edit-appareil.component';
+
+
+
+const appRoutes: Routes = [
+
+  { path: 'appareils', canActivate: [AuthGuard], component: AppareilViewComponent },
+  { path: 'appareils/:id', canActivate: [AuthGuard], component: SingleAppareilComponent },
+  { path: 'auth', component: AuthComponent },
+  { path: '', component: AppareilViewComponent },
+  { path: 'not-found', component: FourOhFourComponent },
+  { path: '**', redirectTo: 'not-found' }
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FirstComponent,
+    AppareilComponent,
+    AuthComponent,
+    AppareilViewComponent,
+    SingleAppareilComponent,
+    FourOhFourComponent,
+    EditAppareilComponent,
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes),
+  ],
+  providers: [
+    AppareilService,
+    AuthService,
+    AuthGuard,
+  ],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA
+],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
